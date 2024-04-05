7451016326245551d2240075a2388756cb983716
Shota Ivanidze <shota.ivanidze0@gmail.com>
2024-04-06 02:29:18 +0400

    Added search box with server dropdown selector, changed api route to also include server name dynamically

--- index.js
+++ index.js
@@ -11,19 +11,20 @@ app.listen(PORT, () => {
 
 const api_key = "RGAPI-14800098-0420-45d9-8a26-2067869d31e4";
 
-app.get('/getsummonerdata/:summonerName', async (req, res) => {
-      let summonerName = req.params.summonerName;
-      let url = `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`;
+app.get('/profile/:serverName/:summonerName', async (req, res) => {
+      let summonerName = req.params.summonerName
+      let serverName = req.params.serverName
+      let url = `https://${serverName}1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`;
       console.log(url)
 
       try {
             let response = await fetch(url);
             let summonerData = await response.json();
             res.json(summonerData);
-        } catch (error) {
+      } catch (error) {
             console.error('Error fetching summoner data:', error);
             res.status(500).json({ error: 'Internal Server Error' });
-        }
+      }
 });
 
 app.get('/', (req, res) => {
