1bb8b2b51f8071c3de9daafa3bf32656cf9a97c2
Shota Ivanidze <shota.ivanidze0@gmail.com>
2024-04-06 01:28:05 +0400

    Improved code style and fixed express route not being fully async

--- index.js
+++ index.js
@@ -1,45 +1,31 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
 const PORT = 3000;
 
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
 });
-
-
-
 
 const api_key = "RGAPI-14800098-0420-45d9-8a26-2067869d31e4";
 
-
-
-
 app.get('/getsummonerdata/:summonerName', async (req, res) => {
-      let summonerData;
       let summonerName = req.params.summonerName;
       let url = `https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${api_key}`;
       console.log(url)
 
-    
-
-      let response = await fetch(url)
-      .then(response => response.json())
-      .then(data => {
-            summonerData = data;
-            console.log(summonerData)
-
-      })
-      
-      res.json(summonerData)
-      
-      console.log(summonerData)
-      
+      try {
+            let response = await fetch(url);
+            let summonerData = await response.json();
+            res.json(summonerData);
+        } catch (error) {
+            console.error('Error fetching summoner data:', error);
+            res.status(500).json({ error: 'Internal Server Error' });
+        }
 });
 
 app.get('/', (req, res) => {
       res.sendFile(__dirname + '/index.html');
-  });
+});
